572107a9134dab314e1e2030d8055e1e34737952
Nik <n.kolesnikov@hotmail.com>
2020-10-19 22:05:52 +0300

    Initial Commit

---
+++ SetGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SetGame
+//
+//  Created by Nik on 19.10.2020.
+//  Copyright © 2020 Nik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
